115f728392a11d9ecf16229a537bc2ddd55ea67d
Lavina <luvina1009@gmail.com>
2023-12-15 01:21:58 +0530

    vercel url export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['localhost', process.env.VERCEL_URL],
-    }
+    },
+    env: {
+        VERCEL_URL: process.env.VERCEL_URL,
+    },
 }
 
 module.exports = nextConfig
